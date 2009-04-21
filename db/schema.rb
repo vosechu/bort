cb623accfca8a87e6a8d0b538cfc6274f6c264ec
Jay McGavren <jay@mcgavren.com>
2009-04-20 22:06:37 -0700

    Application controller renamed per Rails 2.3 convention. More restful_authentication to Authlogic conversion. asset_packager updated.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080929171348) do
+ActiveRecord::Schema.define(:version => 20090421043149) do
 
   create_table "open_id_authentication_associations", :force => true do |t|
     t.integer "issued"
@@ -26,14 +26,6 @@ ActiveRecord::Schema.define(:version => 20080929171348) do
     t.string  "salt",       :null => false
   end
 
-  create_table "passwords", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "reset_code"
-    t.datetime "expiration_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "roles", :force => true do |t|
     t.string "name"
   end
@@ -43,33 +35,26 @@ ActiveRecord::Schema.define(:version => 20080929171348) do
     t.integer "user_id"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "login",                     :limit => 40
-    t.string   "identity_url"
-    t.string   "name",                      :limit => 100, :default => ""
-    t.string   "email",                     :limit => 100
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
-    t.string   "remember_token",            :limit => 40
-    t.string   "activation_code",           :limit => 40
-    t.string   "state",                                    :default => "passive", :null => false
-    t.datetime "remember_token_expires_at"
-    t.datetime "activated_at"
-    t.datetime "deleted_at"
+    t.string   "login",                              :null => false
+    t.string   "email",                              :null => false
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
+    t.string   "single_access_token",                :null => false
+    t.string   "perishable_token",                   :null => false
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
